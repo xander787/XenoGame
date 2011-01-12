3ee3dd2b8263247775ab2418138fce9a8655aa20
James Linnell <jman012guy@James-Linnells-MacBook-Pro.local>
2011-01-11 23:24:05 -0800

    Added in first version of the bullet class, only supports rgualr bullets for the time being

--- Entities/Projectiles/AbstractProjectile.h
+++ Entities/Projectiles/AbstractProjectile.h
@@ -14,10 +14,34 @@
 
 #import <Foundation/Foundation.h>
 #import "PhysicalObject.h"
+#import "ParticleEmitter.h"
+#import "Polygon.h"
 
+typedef enum _BulletID {
+    kBulletID_Normal = 0,
+    kBulletID_Missile,
+    kBulletID_Wave
+} BulletID;
 
 @interface AbstractProjectile : PhysicalObject {
+    Polygon *polygon;
+    Vector2f *collisionPoints;
 
+    int bulletAngle;
+    int bulletSpeed;
+    BulletID mainBulletID;
+    
+    
+    CGPoint turretPosition;
+    
+    CGPoint currentLocation;
+    CGPoint desiredLocation;
+    
+    ParticleEmitter *emitter;
 }
+
+- (id)initWithBulletID:(BulletID)aBulletID fromTurretPosition:(CGPoint)aPosition andAngle:(int)aAngle;
+- (void)update:(CGFloat)aDelta;
+- (void)render;
 
 @end
--- Entities/Projectiles/AbstractProjectile.m
+++ Entities/Projectiles/AbstractProjectile.m
@@ -17,4 +17,165 @@
 
 @implementation AbstractProjectile
 
+
+- (id)initWithBulletID:(BulletID)aBulletID fromTurretPosition:(CGPoint)aPosition andAngle:(int)aAngle {
+    if(self = [super init]){
+        
+        mainBulletID = aBulletID;
+        turretPosition = aPosition;
+        bulletAngle = aAngle;
+        currentLocation = turretPosition;
+        
+        //Paths for main dictionary
+        NSBundle *bundle = [NSBundle mainBundle];
+        NSString *path = [[NSString alloc] initWithString:[bundle pathForResource:@"Bullets" ofType:@"plist"]];
+        NSMutableDictionary *bulletsDictionaryPlist = [[NSMutableDictionary alloc] initWithContentsOfFile:path];
+        NSMutableDictionary *bulletDictionary;
+        [bundle release];
+        [path release];
+        
+        
+        //Load from Plist appropriate bullet type information
+        switch(mainBulletID){
+            case kBulletID_Normal:
+                bulletDictionary = [[NSMutableDictionary alloc] initWithDictionary:[bulletsDictionaryPlist objectForKey:@"kBulletID_Normal"]];
+                break;
+            case kBulletID_Missile:
+                bulletDictionary = [[NSMutableDictionary alloc] initWithDictionary:[bulletsDictionaryPlist objectForKey:@"kBulletID_Missile"]];
+                break;
+            case kBulletID_Wave:
+                bulletDictionary = [[NSMutableDictionary alloc] initWithDictionary:[bulletsDictionaryPlist objectForKey:@"kBulletID_Wave"]];
+            default:
+                break;
+        }
+        [bulletsDictionaryPlist release];
+        
+        
+        bulletSpeed = [[bulletDictionary objectForKey:@"kBulletSpeed"] intValue];
+        
+        
+        //Loop through the collision points, put them in a C array
+        NSArray *tempCollisionPoints = [[NSArray alloc] initWithArray:[bulletDictionary objectForKey:@"kCollisionBoundingPoints"]];
+        collisionPoints = malloc(sizeof(Vector2f) * [tempCollisionPoints count]);
+        bzero(collisionPoints, sizeof(Vector2f) * [tempCollisionPoints count]);
+        for(int i = 0; i < [tempCollisionPoints count]; i++){
+            NSArray *coords = [[NSArray alloc] initWithArray:[[tempCollisionPoints objectAtIndex:i] componentsSeparatedByString:@","]];
+            @try {
+                collisionPoints[i] = Vector2fMake([[coords objectAtIndex:0] intValue], [[coords objectAtIndex:1] intValue]);
+            }
+            @catch (NSException * e) {
+                NSLog(@"Exception thrown: %@", e);
+            }
+            @finally {
+                NSLog(@"Bullet Coll Coord: %f, %f", collisionPoints[i].x, collisionPoints[i].y);
+            }
+            [coords release];
+        }
+        
+        
+        //Decides how to setup the particle emitter
+        switch(mainBulletID){
+            case kBulletID_Normal:
+                emitter = [[ParticleEmitter alloc] initParticleEmitterWithImageNamed:[bulletDictionary objectForKey:@"kBulletImage"] 
+                                                                            position:Vector2fMake(currentLocation.x, currentLocation.y) 
+                                                              sourcePositionVariance:Vector2fZero 
+                                                                               speed:0.5
+                                                                       speedVariance:0.0 
+                                                                    particleLifeSpan:0.01
+                                                            particleLifespanVariance:0.1
+                                                                               angle:bulletAngle 
+                                                                       angleVariance:0.0 
+                                                                             gravity:Vector2fZero 
+                                                                          startColor:Color4fMake(0.0, 1.0, 1.0, 1.0) 
+                                                                  startColorVariance:Color4fMake(0.0, 0.0, 0.0, 0.0) 
+                                                                         finishColor:Color4fMake(1.0, 1.0, 1.0, 1.0) 
+                                                                 finishColorVariance:Color4fMake(0.0, 0.0, 0.0, 0.0) 
+                                                                        maxParticles:100
+                                                                        particleSize:10.0
+                                                                  finishParticleSize:10.0
+                                                                particleSizeVariance:0.0
+                                                                            duration:-1.0
+                                                                       blendAdditive:NO];
+                break;
+                
+            case kBulletID_Missile:
+                break;
+                
+            case kBulletID_Wave:
+                break;
+                
+            default:
+                break;
+        }
+        
+        
+        //Allocate polygon with previously made collision points
+        polygon = [[Polygon alloc] initWithPoints:collisionPoints andCount:[tempCollisionPoints count] andShipPos:CGPointMake(turretPosition.x, turretPosition.y)];
+        
+        
+        [tempCollisionPoints release];
+        [bulletDictionary release];
+    }
+    
+    return self;
+}
+
+- (void)update:(CGFloat)aDelta {
+    
+    //Moves the position to follow the angle of trajectory, mostly taken fomr the ParticleEmitter code
+    float newAngle = (GLfloat)DEGREES_TO_RADIANS(bulletAngle);
+    CGPoint vector = CGPointMake(cosf(newAngle), sinf(newAngle));
+    currentLocation.x += (bulletSpeed * 10) * aDelta * vector.x;
+    currentLocation.y += (bulletSpeed * 10) * aDelta * vector.y;
+    [emitter setSourcePosition:Vector2fMake(currentLocation.x, currentLocation.y)];
+    
+    
+    //Refresh the position of our polygon to move with the particle
+    [polygon setPos:currentLocation];
+    
+    [emitter update:aDelta];
+}
+
+- (void)render {
+    [emitter renderParticles];
+    
+    //From PlayerShip class
+    if(DEBUG) {                
+        glPushMatrix();
+        
+        glColor4f(1.0f, 1.0f, 1.0f, 1.0f);
+        
+        //Loop through the all the lines except for the last
+        for(int i = 0; i < (polygon.pointCount - 1); i++) {
+            GLfloat line[] = {
+                polygon.points[i].x, polygon.points[i].y,
+                polygon.points[i+1].x, polygon.points[i+1].y,
+            };
+            
+            glVertexPointer(2, GL_FLOAT, 0, line);
+            glEnableClientState(GL_VERTEX_ARRAY);
+            glDrawArrays(GL_LINES, 0, 2);
+        }
+        
+        
+        //Renders last line, we do this because of how arrays work.
+        GLfloat lineEnd[] = {
+            polygon.points[([polygon pointCount] - 1)].x, polygon.points[([polygon pointCount] - 1)].y,
+            polygon.points[0].x, polygon.points[0].y,
+        };
+        
+        glVertexPointer(2, GL_FLOAT, 0, lineEnd);
+        glEnableClientState(GL_VERTEX_ARRAY);
+        glDrawArrays(GL_LINES, 0, 2);
+        
+        glPopMatrix();
+    }
+}
+
+- (void)dealloc {
+    [polygon release];
+    free(collisionPoints);
+    [super dealloc];
+}
+
 @end
