c62477c1d30fc6c9806dc0e3642caa009c8e50c0
Alexander <alexander@alexanders-macbook-pro.local>
2011-07-15 19:30:00 -0700

    Added the files for custom boss ship programming to the project

---
+++ Entities/Ships/Bosses/BossShipEos.m
@@ -0,0 +1,20 @@
+//
+//  BossShipEos.m
+//  Xenophobe
+//
+//  Created by Alexander on 10/20/10.
+//  Copyright 2010 Alexander Nabavi-Noori, XanderNet Inc. All rights reserved.
+//  
+//  Team:
+//  Alexander Nabavi-Noori - Software Engineer, Game Architect
+//	James Linnell - Software Engineer, Creative Design, Art Producer
+//	Tyler Newcomb - Creative Design, Art Producer
+//
+
+#import "BossShipEos.h"
+#import "BossShip.h"
+
+
+@implementation BossShipEos
+
+@end
---
+++ Entities/Ships/Bosses/BossShipThemis.h
@@ -0,0 +1,21 @@
+//
+//  BossShipThemis.h
+//  Xenophobe
+//
+//  Created by Alexander on 10/20/10.
+//  Copyright 2010 Alexander Nabavi-Noori, XanderNet Inc. All rights reserved.
+//  
+//  Team:
+//  Alexander Nabavi-Noori - Software Engineer, Game Architect
+//	James Linnell - Software Engineer, Creative Design, Art Producer
+//	Tyler Newcomb - Creative Design, Art Producer
+//
+
+#import <Foundation/Foundation.h>
+#import "BossShip.h"
+
+@interface BossShipThemis : BossShip {
+    
+}
+
+@end
